e7187b7de055b98c4a1ca5cf4b44a76f4a4e53f0
lupeng918 <31698459+lupeng918@users.noreply.github.com>
2017-09-10 13:13:12 +0800

    Add a comment
    I add a comment in source file

--- scanf-test.c
+++ scanf-test.c
@@ -1,9 +1,12 @@
+/*连续两个scanf函数，如何输入两个数值？ubuntu gcc 的输出始终为0
+ *此程序在windows codeblocks 里编译，无论怎么输入，最终的输出都是496
+ */
 #include <stdio.h>
 int main (void)
 {
 	int a, b, c, d;
 	scanf("please input a:%d", &a);
-	scanf("please input b:%d", &b); /**连续两个scanf函数，如何输入两个数值？此程序得输出始终为0*****/
+	scanf("please input b:%d", &b); 
          	c = a *b;
 	printf("Result of c is :%d.\n", c);
 	return 0;
